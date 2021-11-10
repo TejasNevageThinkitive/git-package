3551b57b38f0c564319cc4fcfea320e0210a9a35
Tejas <tejas.nevage@thinkitive.com>
2021-11-10 21:41:55 +0530

    first commit

--- src/main/java/com/example/demo/GitpackageApplication.java
+++ src/main/java/com/example/demo/GitpackageApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.example.demo.unit.Test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,8 @@ public class GitpackageApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitpackageApplication.class, args);
+		Test test = new Test();
+		test.display("hi");
 	}
 
 }
